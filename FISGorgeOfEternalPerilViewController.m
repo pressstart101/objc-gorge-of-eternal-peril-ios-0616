ea4666d42760fae3a71b96b9be7f5584d598e65e
pressstart101 <walterscott404@yandex.com>
2016-06-18 14:00:40 -0400

    done

--- FISGorgeOfEternalPerilViewController.m
+++ FISGorgeOfEternalPerilViewController.m
@@ -3,5 +3,8 @@
 #import "FISGorgeOfEternalPerilViewController.h"
 
 @implementation FISGorgeOfEternalPerilViewController
+- (IBAction)acceptYourFateTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
